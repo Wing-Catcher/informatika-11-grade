d6f461239a07330b44a75caade45465e5f6c70da
Wing-Catcher <catcherwing@gmail.com>
2025-10-07 08:27:27 +0700

    new file:   Latihan Dasar 17 Raffy XI D.cpp 	new file:   Latihan Dasar 18 Raffy XI D.cpp 	new file:   Latihan Dasar 19 Raffy XI D.cpp 	modified:   outputs/Latihan Dasar 15 Raffy XI D.exe 	new file:   outputs/Latihan Dasar 17 Raffy XI D.exe 	new file:   outputs/Latihan Dasar 18 Raffy XI D.exe 	new file:   outputs/Latihan Dasar 19 Raffy XI D.exe

---
+++ Latihan Dasar 18 Raffy XI D.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "SAYA SEDANG BELAJAR KODING" << endl;
+    cout << "SAYA PASTI BISA" << endl;
+    for(int i = 1; i <= 3; i++){
+        cout << i << ". SAYA SEDANG BELAJAR KODING" << endl;
+        cout << "SAYA PASTI BISA" << endl;
+    }
+    return 0;
+}
---
+++ Latihan Dasar 19 Raffy XI D.cpp
@@ -0,0 +1,16 @@
+/*
+Nama file       : Latihan Dasar 19 Raffy XI D.cpp
+Judul Program   : Perulangan dengan while
+Isi Program     : Menampilkan urutan abjad kebawah A..Z menggunakan perulangan while
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    char i = 'A';
+    while(i <= 'Z'){
+        cout << i << endl;
+        i++;
+    }
+    return 0;
+}
