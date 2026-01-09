b6f960d238d7cae67a728aeccad8f19a09b054f7
Wing Catcher <163217669+Wing-Catcher@users.noreply.github.com>
2026-01-09 13:42:27 +0700

    just an update

---
+++ Latihan Dasar 25 Raffy XI D.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char nilai;
+    cout << "Masukkan nilai: "
+    cin >> nilai;
+    cout << endl;
+    switch (nilai){
+        case 'A':
+            cout << "Pertahankan!" << endl;
+            break;
+        case 'B':
+            cout << "Harus lebih baik lagi!" << endl;
+            break;
+        case 'C':
+            cout << "Perbanyak belajar!" << endl;
+            break;
+        case 'D':
+            cout << "Jangan keseringan main" << endl;
+            break;
+        case 'E':
+            cout << "Kebanyakan bolos.." << endl;
+            break;
+        default:
+            cout << "Maaf, format nilai tidak sesuai" << endl;
+            break;
+    }
+    return 0;
+}
